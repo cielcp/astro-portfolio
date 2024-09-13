86afa076d71aafb1062e155c80b8b7d4dbf0d905
cielcp <cielcpark@gmail.com>
2024-09-13 03:22:57 -0400

    Started rebrand, finished home hero

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,24 @@
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    theme: {
-        extend: {
-            keyframes: {
-                animatedgradient: {
-                    '0%': { backgroundPosition: '0% 50%' },
-                    '50%': { backgroundPosition: '100% 50%' },
-                    '100%': { backgroundPosition: '0% 50%' },
-                },
-            },
-            backgroundSize: {
-                '300%': '300%',
-            },
-            animation: {
-                gradient: 'animatedgradient 10s ease infinite alternate',
-            },
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      keyframes: {
+        animatedgradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
         },
+      },
+      backgroundSize: {
+        '300%': '300%',
+      },
+      animation: {
+        gradient: 'animatedgradient 10s ease infinite alternate',
+      },
+      fontFamily: {
+        readex: ['Readex Pro', 'sans-serif'],
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 }
